4b35662aa8c6a4ab80b87c03465ae621a9cbbfb4
Chen Shun <chenshun87@163.com>
2012-05-19 11:55:13 +0800

    1.add CoreEngine class

---
+++ NGL/NGLEngine/CoreEngine.cpp
@@ -0,0 +1,9 @@
+//
+//  CoreEngine.cpp
+//  NGL_Test
+//
+//  Created by chenshun on 12-5-19.
+//  Copyright 2012年 chenshun. All rights reserved.
+//
+
+#include "CoreEngine.h"
---
+++ NGL/NGLEngine/CoreEngine.h
@@ -0,0 +1,14 @@
+//
+//  CoreEngine.h
+//  NGL_Test
+//
+//  Created by chenshun on 12-5-19.
+//  Copyright 2012年 chenshun. All rights reserved.
+//
+
+#ifndef NGL_Test_CoreEngine____H___
+#define NGL_Test_CoreEngine____H___
+
+
+
+#endif
